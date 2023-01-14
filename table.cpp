d1435e308fed3eba3922b8f114998f4cb4bf3ff8
Abhishek verma <122609978+abhishekverma-6@users.noreply.github.com>
2023-01-14 23:38:49 +0530

    Add files via upload

---
+++ table.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int n,r,s;
+	printf("the number is armstrong\n");
+	for(n=1;n<+1000;n++){
+	s=0;
+	while(x!=0){
+	x=x%10;
+	s=s+r*r*r;
+	x=x/10;
+}
+	if()
+	
+	printf("%d\n",n);
+}
+	
+	
+	
+	
+}
